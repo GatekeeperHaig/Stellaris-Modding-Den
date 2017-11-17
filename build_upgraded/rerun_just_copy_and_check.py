48d90d01ffa4e2dfad1239482d893cc75fcbb7da
Gra-tak <nemesissp@gmx.de>
2017-11-17 13:40:16 +0100

    Changed the way the output path is used for non building files. Now has to be given including subfolders. No change for ExO mod as it was already like this here

--- build_upgraded/rerun_just_copy_and_check.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-import subprocess
-import os
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
-os.chdir('..')
-subprocess.call('python ./createUpgradedBuildings.py "C:/SteamLibrary/SteamApps/common/Stellaris/common/scripted_effects/00_scripted_effects.txt" "--tags" "Builings,Utilities,Fixes" "--picture_file" "thumb_direct_build.png" "--custom_mod_name" "Build Upgraded - Direct construction of high tier buildings" "--just_copy_and_check"', shell=True)
