6217c3359dd042c15d153fb46c8defb4a01c0fb1
Gra-tak <nemesissp@gmx.de>
2017-12-14 00:01:29 +0100

    Fixed armor values (ships AND stations). Higher vals on architecture. Calculated in the removal of t6 armor. Fixed weights. GUI: Fixed filter button missing.

--- combat-overhaul-beta/armorValues.py
+++ combat-overhaul-beta/armorValues.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 
@@ -14,17 +14,21 @@ import numpy as np
 
 def main():
   
-  tiersArmor=[0,1,2,3,4,5]
-  for i in range(len(tiersArmor)):
-    tiersArmor[i]=float(tiersArmor[i])/5.0*4.0+1
+  # tiersArmor=[0,1,2,3,4,5]
+  # for i in range(len(tiersArmor)):
+  #   tiersArmor[i]=float(tiersArmor[i])/5.0*4.0+1
   tiersArch=range(1,6)
+  tiersArmor=range(1,6)
   #tiersArch=np.linspace(1, 5)
-  archpart=1./5.
+  archpart=0.3
   bsMin=40
   bsMax=560
-  order=[3,0,2,1,4] #alphabetical
-  
+  # order=[3,0,2,1,4] #alphabetical
+  order=[0,1,2,3,4]
+  order=[0,2,4,4] #station
+
   shipsizes=[1,2,4,8,16]
+  shipsizes=[6,6,12,12,16] #station
   maxArmor=70
   givenValues=[[[1
   ,1.5,5],[0.6,0.655,0.9]],[[1,2,5],[0.55,0.705,0.925]],[[1,3,5],[0.5,0.82,0.95]],[[1,4,5],[0.45,0.91,0.975]],[[1,4.5,5],[0.4,0.95,1]]]
