91837d7a530d43c462539115e8fb876b72fc5977
Gra-tak <nemesissp@gmx.de>
2018-03-12 14:41:44 +0100

    extended create ai vars from modifiers

--- pythonScripts/createAIVarsFromModifiers.py
+++ pythonScripts/createAIVarsFromModifiers.py
@@ -12,6 +12,9 @@ def parse(argv):
   parser = argparse.ArgumentParser(description="", formatter_class=RawTextHelpFormatter)
   parser.add_argument('inputfileName', )
   parser.add_argument('outputfileName', )
+  parser.add_argument('-n','--effect_name', default='check_planet_modifier', help="The name that will be given to the generated scripted_effect")
+  parser.add_argument('-t','--type', default="building", help="Type of objects that are used: 'has_<value>' will be the check. Not needed for planet based modifiers. Reasonable values are 'building'(default)/'blocker' and possible others")
+  # parser.add_argument('-a','--adjacency',action="store_true", help="qu")
   addCommonArgs(parser)
   
   
@@ -30,7 +33,7 @@ def main(args):
   inputTagList=TagList(0)
   inputTagList.readFile(args.inputfileName,args, varsToValue)
   outTagList=TagList(0)
-  outTagList.add("check_planet_modifiers", TagList(1))
+  outTagList.add(args.effect_name, TagList(1))
   # outTagTemplate=TagList(2)
   # outTagTemplate.add("limit", TagList(3).add("has_modifier","toFill"))
   # outTagTemplate.add("change_variable", TagList(3).add("which","toFill").add("value","toFill"))
@@ -41,16 +44,29 @@ def main(args):
       continue
     # val.printAll()
     newCheck=TagList(2).add("limit", TagList(3).add("has_modifier",name))
+    newCheckBuildingModifier=TagList(2).add("limit", TagList(3).add("has_"+args.type,name))
+    newCheckAdjModifier=TagList(2).add("limit", TagList(3).add("has_"+args.type,name)).add("prev",TagList(3))
+
     for possibleModifierName, pmVal in val.getAll():
       if "tile_resource_" in possibleModifierName:
-        # newCheck=copy.deepcopy(outTagTemplate)
-        # newCheck.get("limit").replace("has_modifier",name)
         newCheck.add("change_variable", TagList(3).add("which",possibleModifierName.replace("tile_resource_","")).add("value",pmVal))
-        # newCheck.get("change_variable").replace("which", possibleModifierName.replace("tile_resource_",""))
-        # newCheck.get("change_variable").replace("value", pmVal)
-        # outTagList.get("check_planet_modifiers").add("if", newCheck)
+    if "planet_modifier" in val.names:
+      # print("NAME"+name)
+      # val.printAll()
+      for possPlanetModName, ppmVal in val.get("planet_modifier").getAll():
+       if "tile_resource_" in possPlanetModName:
+         newCheckBuildingModifier.add("change_variable", TagList(3).add("which",possPlanetModName.replace("tile_resource_","")).add("value",ppmVal))
+    if "adjacency_bonus" in val.names:
+      for possPlanetModName, ppmVal in val.get("adjacency_bonus").getAll():
+        if "tile_building_resource_" in possPlanetModName:
+          newCheckAdjModifier.get("prev").add("change_variable", TagList(3).add("which",possPlanetModName.replace("tile_building_resource_","")).add("value",ppmVal))
+
     if len(newCheck.names)>1:
-      outTagList.get("check_planet_modifiers").add("if", newCheck)
+      outTagList.get(args.effect_name).add("if", newCheck)
+    if len(newCheckBuildingModifier.names)>1:
+      outTagList.get(args.effect_name).add("if", newCheckBuildingModifier)
+    if len(newCheckAdjModifier.get("prev").names)>0:
+      outTagList.get(args.effect_name).add("every_neighboring_tile", newCheckAdjModifier)
 
   # outTagList.printAll()
   with open(args.outputfileName,'w') as file:
