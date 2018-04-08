efc0cb380b37d32b1bd0b1c9517b47695b740a13
Gra-tak <nemesissp@gmx.de>
2018-04-09 00:46:49 +0200

    fix in short form (one-line-level 2)

--- pythonScripts/fix.py
+++ pythonScripts/fix.py
@@ -5,7 +5,8 @@ import sys, argparse, subprocess,os
 from argparse import RawTextHelpFormatter
 import math
 from stellarisTxtRead import *
+from custom_difficulty_files import *
 
 tagList=TagList().readFile("../cgm_buildings_script_source/common/buildings/cgm_new_building_content_tile_restricted_resource_buildings.txt",0)
 with open("test.txt",'w') as file:
-  tagList.writeAll(file)
+  tagList.writeAll(file, args())
