41c27c2fbc8b337b55564ff4bf3c5e97d1d22ae7
Gra-tak <nemesissp@gmx.de>
2018-04-08 13:11:43 +0200

    wip

--- pythonScripts/cgm_files.py
+++ pythonScripts/cgm_files.py
@@ -12,26 +12,22 @@ from functools import reduce
 
 debugMode=True
 
-changeStepYears=[5,4,3,2,1]
-changeSteps = [50, 25, 10, 5, 1]
+changeSteps = [50, 20, 1]
 for s in reversed(changeSteps):
   changeSteps.append(-s)
-for s in reversed(changeStepYears):
-  changeStepYears.append(-s)
-boniNames=["capital_building","empire_unique_building","planet_unique_building","military_building","standard_resource_building","research_resource_building","unity_resource_building","special_resource_building","replicator_building", "all"]
-boniPictures=["GFX_CGM_buildings_menu" for entry in boniNames]
-buildSpeedBonus=[[entry+"_building_time_mult"] for entry in boniNames if entry !="all"]
-buildCostBonus=[[entry+"_building_cost_mult"] for entry in boniNames if entry !="all"]
-buildSpeedBonus.append(reduce(lambda x,y: x+y, buildSpeedBonus))
-buildCostBonus.append(reduce(lambda x,y: x+y, buildCostBonus))
+bonusNames=["capital_building","empire_unique_building","planet_unique_building","military_building","standard_resource_building","research_resource_building","unity_resource_building","special_resource_building","replicator_building", "all"]
+bonusPictures=["GFX_CGM_buildings_menu" for entry in bonusNames]
+cats=["building_time_mult","building_cost_mult"]
+# buildSpeedBonus=[[entry+"_building_time_mult"] for entry in bonusNames if entry !="all"]
+# buildCostBonus=[[entry+"_building_cost_mult"] for entry in bonusNames if entry !="all"]
+# buildSpeedBonus.append(reduce(lambda x,y: x+y, buildSpeedBonus))
+# buildCostBonus.append(reduce(lambda x,y: x+y, buildCostBonus))
 
 # possibleBoniPictures=["GFX_evt_mining_station","GFX_evt_dyson_sphere","GFX_evt_animal_wildlife", "GFX_evt_think_tank", "GFX_evt_unity_symbol","GFX_evt_arguing_senate","GFX_evt_hangar_bay", "GFX_evt_debris", "GFX_evt_sabotaged_ship","GFX_evt_pirate_armada","GFX_evt_fleet_neutral","GFX_evt_city_ruins","GFX_evt_metropolis"]
 
 
 
 
-print(buildSpeedBonus)
-print(buildCostBonus)
 
 # # doTranslation=True
 # doTranslation=False
@@ -63,8 +59,46 @@ print(buildCostBonus)
 
 
 eventNameSpace="core_game_mechanics_and_ai_base.{!s}"
+eventNames="core_game_mechanics_and_ai_base_{!s}"
 
 name_mainMenuEvent="core_game_mechanics_and_ai_base.10"
-id_Change_Speed=20  #reserved range up to 29
-id_Change_Cost=30  #reserved range up to 39
+id_subMainMenuEvent=11
+id_Change=[20,30]  #reserved range up to 39
 
+
+buildingOptionsFile=TagList("namespace","core_game_mechanics_and_ai_base")
+mainMenu=TagList("id", name_mainMenuEvent)
+mainMenu.add("is_triggered_only", "yes")
+mainMenu.add("name", eventNames.format("main_event.name"))
+mainMenu.add("desc", eventNames.format("main_event.desc"))
+mainMenu.add("picture", "GFX_CGM_buildings_menu")
+buildingOptionsFile.addComment("main menu")
+buildingOptionsFile.add("country_event", mainMenu)
+for catI,cat in enumerate(cats):
+  mainSubMenu=TagList("id", eventNameSpace.format(id_subMainMenuEvent+catI))
+  mainSubMenu.add("is_triggered_only", "yes")
+  mainSubMenu.add("name", eventNames.format(cat+"_event.name"))
+  mainSubMenu.add("desc", eventNames.format(cat+"_event.desc"))
+  mainSubMenu.add("picture", "GFX_CGM_buildings_menu")
+  buildingOptionsFile.addComment(cat)
+  buildingOptionsFile.add("country_event", mainSubMenu)
+  mainMenu.add("option", TagList("name", eventNames.format(cat)+"_option.name").add("hidden_effect", TagList("country_event", TagList("id",eventNameSpace.format(id_subMainMenuEvent+catI)))))
+  bonuses=[[entry+"_"+cat] for entry in bonusNames if entry !="all"]
+  bonuses.append(reduce(lambda x,y: x+y, bonuses))
+  for bonusI,bonus,bonusName in zip(range(len(bonuses)),bonuses, bonusNames):
+    bonusMenu=TagList("id", eventNameSpace.format(id_Change[catI]+bonusI))
+    bonusMenu.add("is_triggered_only", "yes")
+    bonusMenu.add("name", eventNames.format("{}_{}_event.name".format(cat, bonusName)))
+    bonusMenu.add("desc", eventNames.format("{}_{}_event.desc".format(cat, bonusName)))
+    bonusMenu.add("picture", "GFX_CGM_buildings_menu")
+    buildingOptionsFile.addComment(bonusName+" "+cat)
+    buildingOptionsFile.add("country_event", bonusMenu)
+    mainSubMenu.add("option", TagList("name", eventNames.format("{}_{}_event_option.name".format(cat, bonusName))).add("hidden_effect", TagList("country_event", TagList("id",eventNameSpace.format(id_Change[catI]+bonusI)))))
+    for changeStep in changeSteps:
+      bonusMenu.add("option", TagList("name", eventNames.format("change_"+str(changeStep).replace("-", "neg"))))
+    bonusMenu.add("option", TagList("name", "BACK").add("hidden_effect", TagList("country_event", TagList("id",eventNameSpace.format(id_subMainMenuEvent+catI)))))
+  mainSubMenu.add("option", TagList("name", "BACK").add("hidden_effect", TagList("country_event", TagList("id",name_mainMenuEvent))))
+
+
+# buildingOptionsFile.printAll()
+outputToFolderAndFile(buildingOptionsFile, "events", "cgm_buildings_modifiers.txt", level=2, modFolder="../cgm_buildings_script_source")
