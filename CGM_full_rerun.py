5dbcd51bef9e2497178fc89de8b58dcddb5e8f0e
Gra-tak <nemesissp@gmx.de>
2018-04-20 13:26:03 +0200

    applied script

--- CGM_full_rerun.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-import subprocess
-import os
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
-subprocess.call('python pythonScripts/createUpgradedBuildings.py "cgm_buildings.mod" "-m" "--output_folder" "CGM/buildings" "--one_line_level" "2" "--custom_mod_name" "!Core Game Mechanics: Buildings" "--make_optional" "--scripted_variables" "cgm_buildings_script_source/common/scripted_variables/cgm_variables.txt"', shell=True)
