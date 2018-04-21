23e54ba9bc34a11669c4cfb6a4e2dc6bcde77347
Gra-tak <nemesissp@gmx.de>
2018-04-21 23:50:55 +0200

    added script to join the scripts

---
+++ CGM/buildings_full_rerun.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import subprocess
+import os
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+subprocess.call('python ../pythonScripts/locList.py --create_main_file --output_folder "buildings/localisation/" buildings_script_source/localisation/english/cgm_building_l_english.yml', shell=True)
+subprocess.call('python ../pythonScripts/locList.py --output_folder "buildings/localisation/" buildings_script_source/localisation/french/cgm_building_l_french.yml', shell=True)
+subprocess.call('python buildings_rerun.py', shell=True)
+subprocess.call('python buildings/localisation/cgm_building_main.py', shell=True)
