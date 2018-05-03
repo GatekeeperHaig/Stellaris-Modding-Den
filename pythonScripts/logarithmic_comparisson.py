54c7796cddc22648024d9b19e7bbf436847a4c8a
Gra-tak <nemesissp@gmx.de>
2018-05-03 10:48:03 +0200

    resolves #16

--- pythonScripts/logarithmic_comparisson.py
+++ pythonScripts/logarithmic_comparisson.py
@@ -13,7 +13,7 @@ def main():
   out=TagList()
   current=out.addReturn("while")
   for i in reversed(range(12)):
-    ifLoc=current.add("limit",variableOpNew("check", "reserve_transport", pow(2,i)-0.001, ">"))
+    ifLoc=current.add("limit",variableOpNew("check", "reserve_transport", pow(2,i)-0.001, ">"))#.add("minerals", pow(2,i)-0.001, "", ">"))
     ifLoc.add("add_minerals", -pow(2,i))
     ifLoc.variableOp("change", "reserve_transport", -pow(2,i))
     current=out.addReturn("if")
