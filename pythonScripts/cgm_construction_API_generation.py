bd14715f405e8550a6b3210264b5e37607d4c83e
Gra-tak <nemesissp@gmx.de>
2018-05-14 00:25:59 +0200

    adding new files (outside any mod)

---
+++ pythonScripts/cgm_construction_API_generation.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys, os, io,math
+from cgm_automation_files import *
+
+def main():
+
+  os.chdir(os.path.dirname(__file__))
+
+  createEffectDecisionStuff()
+  return 
+  automatedCreationAutobuildAPI(resources)
+  automatedCreationAutobuildAPI(resources,"alphamod",["../NOTES/api files/cgm_api_files/alphamod/"])
+
+
+if __name__ == "__main__":
+  main()
