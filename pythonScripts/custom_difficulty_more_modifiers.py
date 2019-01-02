5ad4fc779e60a13aa0673a9187b5618b1d42c0ec
Gratak <nemesissp@gmx.de>
2019-01-02 23:07:03 +0100

    modifiers now applied, next step:include randomness

--- pythonScripts/custom_difficulty_more_modifiers.py
+++ pythonScripts/custom_difficulty_more_modifiers.py
@@ -20,6 +20,7 @@ name_gameStartFireOnlyOnce=eventNameSpace.format(0)
 name_mainMenuEvent=eventNameSpace.format(1)
 name_rootUpdateEvent=eventNameSpace.format(2)
 id_groupEvents=10 #reserved to 19
+id_updateEvents=20 #reserved to 21
 id_modifierEventMenus=100 #reserved to 199
 
 # t_notLockedTrigger=TagList("not", TagList("has_global_flag", "custom_difficulty_locked"))
@@ -35,6 +36,7 @@ t_closeOption=TagList("name", "custom_difficulty_closeMM").add("hidden_effect", 
 def main():
   os.chdir(os.path.dirname(os.path.abspath(__file__)))
   # debugMode=False
+  #todo: make normal custom difficulty leave menu event call MM update and vise versa
 
   locList=LocList()
   cdf.globalAddLocs(locList)
@@ -47,6 +49,7 @@ def main():
   staticModifierFile=TagList()
   # groupFileContent=TagList("namespace", eventNameSpace.format("")[:-1])
   modifierMenuFileContent=TagList("namespace", eventNameSpace.format("")[:-1])
+  updateFileContent=TagList("namespace", eventNameSpace.format("")[:-1])
 
 
 
@@ -89,7 +92,10 @@ def main():
   updateEvent.add("id", name_rootUpdateEvent)
   updateEvent.add("hide_window","yes" )
   updateEvent.add("is_triggered_only", "yes")
-
+  updateImmediate=updateEvent.addReturn("immediate")
+  isPlayable=updateImmediate.addReturn("every_country").add("limit",TagList("OR", TagList("is_country_type","default").add("is_country_type","fallen_empire" ).add("is_country_type","awakened_fallen_empire" ).add("is_country_type","ascended_empire" )))
+  isPlayable.createReturnIf(TagList("is_ai", "yes")).createEvent(eventNameSpace.format(id_updateEvents))
+  isPlayable.addReturn("else").createEvent(eventNameSpace.format(id_updateEvents+1))
 
   curIdGroupEvent=id_groupEvents
   curIdModifierEvent=id_modifierEventMenus
@@ -152,6 +158,51 @@ def main():
   mainMenuEvent.add("option",t_backMainOption)
   mainMenuEvent.add("option",t_closeOption)
 
+  cur_id_updateEvents=id_updateEvents
+  for category in ["AI", "Player"]:
+    localUpdateEvent=updateFileContent.addReturn("country_event")
+    localUpdateEvent.add("id", eventNameSpace.format(cur_id_updateEvents))
+    localUpdateEvent.addComment(category)
+    localUpdateEvent.add("hide_window","yes" )
+    localUpdateEvent.add("is_triggered_only", "yes")
+    localUpdateImmediate=localUpdateEvent.addReturn("immediate")
+    etUpdate=localUpdateImmediate.addReturn(ETMM)
+
+    if category=="AI":
+      localUpdateImmediate.variableOp("set", "custom_difficulty_randomness_factor",1)
+      localUpdateImmediate.variableOp("set", "custom_difficulty_tmp","custom_difficulty_random_handicap")
+      localUpdateImmediate.variableOp("multiply", "custom_difficulty_tmp","custom_difficulty_random_handicap_perc")
+      localUpdateImmediate.variableOp("divide", "custom_difficulty_tmp",100*20) #100 for from perc, 20 as max handicap
+      localUpdateImmediate.variableOp("subtract", "custom_difficulty_randomness_factor","custom_difficulty_tmp")
+
+    for group in groupList:
+      for modifier in group.modifiers:
+        etUpdate.variableOp("set", modifier.modifiername, modifier.modifiername+"_"+category)
+        someThingChanged=localUpdateImmediate.createReturnIf(TagList("NOT",cdf.variableOpNew("check", modifier.modifiername, ETMM)))
+        someThingChanged.variableOp("set", modifier.modifiername, ETMM)
+        removeOld=TagList()
+        addNew=TagList("if", TagList("limit",cdf.variableOpNew("check", modifier.modifiername,0)))
+        # addNew=cdf.createReturnIf(TagList(), cdf.variableOpNew("check", modifier.modifiername,0))
+        # addNewCur=addNew
+        # addNew=cdf.createReturnIf(TagList(),TagList("NOT", cdf.variableOpNew("check", modifier.modifiername,0)))
+        for r,name in zip(modifier.rangeUsed, modifier.modNames):
+          removeOld.add("remove_modifier", name)
+          elseif=addNew.addReturn("else_if")
+          value=modifier.multiplier*(r+0.5)
+          value="{:.3f}".format(value)
+          if modifier.multiplier>0:
+            comp="<"
+          else:
+            comp=">"
+          elseif.add("limit", cdf.variableOpNew("check", modifier.modifiername, value,comp))
+          elseif.add("add_modifier", TagList("modifier", name).add("days",-1))
+        someThingChanged.addTagList(removeOld)
+        someThingChanged.addTagList(addNew)
+
+        #TODO: remove existing modifiers
+
+    cur_id_updateEvents+=1
+
 
 
 
@@ -164,6 +215,7 @@ def main():
   cdf.outputToFolderAndFile(onActions, "common/on_actions", "custom_difficultyMM_on_action.txt",2,"../gratak_mods/custom_difficultyMM")
   cdf.outputToFolderAndFile(mainFileContent , "events", "custom_difficultyMM_main.txt" ,2,"../gratak_mods/custom_difficultyMM")
   cdf.outputToFolderAndFile(modifierMenuFileContent , "events", "custom_difficultyMM_modifier_menus.txt" ,2,"../gratak_mods/custom_difficultyMM")
+  cdf.outputToFolderAndFile(updateFileContent , "events", "custom_difficultyMM_modifier_update.txt" ,2,"../gratak_mods/custom_difficultyMM")
   cdf.outputToFolderAndFile(staticModifierFile , "common/static_modifiers", "custom_difficultyMM.txt" ,2,"../gratak_mods/custom_difficultyMM")
   locList.writeToMod("../gratak_mods/custom_difficultyMM","custom_difficultyMM")
 
@@ -295,6 +347,8 @@ class Modifier:
       self.setUnit("%")
     self.name="$MOD_"+modifier.upper()+"$"
     self.name+="$mod_"+modifier.lower()+"$" #some are lower, some are upper. Game does not complain if something misses so I just add both...
+    # self.values=[]
+    self.modNames=[]
 
   def addMultiplier(self, mult):
     mult=float(mult)
@@ -316,6 +370,9 @@ class Modifier:
 
 
   def toStaticModifierFiles(self, modifierTagList, locList, rangeUsed=range(-2,11)):
+    if 0 in rangeUsed:
+      rangeUsed=list(rangeUsed)
+      rangeUsed.remove(0)
     self.rangeUsed=rangeUsed
     value=self.multiplier;
     if self.unit=="%":
@@ -323,21 +380,25 @@ class Modifier:
     elif self.unit!="" and self.unit!="1":
       print("ERROR: INVALID UNIT")
 
-
+    
+    
     for m in rangeUsed:
-      if m==0:
-        continue
+      # if m==0:
+      #   continue
       if not locList is None:
         modName="custom_difficulty_MM_mod_{}_{!s}".format(self.modifier, m)
+        self.modNames.append(modName)
         modifierList=modifierTagList.addReturn(modName)
         locList.append(modName, "$FE_DIFFICULTY$: "+self.name)
       else:
         modifierList=modifierTagList #used by ModifierSubGroup
+      # self.values.append("{:.3f}".format(value*m))
       modifierList.add(self.modifier,"{:.3f}".format(value*m) )
 
     if not locList is None:
       modOptionName="custom_difficulty_MM_mod_{}".format(self.modifier)
       locList.append(modOptionName, "§B"+self.name+"§!")
+      self.modifiername=modOptionName
       return modOptionName
 
 
@@ -345,11 +406,13 @@ class ModifierSubGroup(Modifier):
   def __init__(self, name=''):
     self.name=name
     self.modifiers=[]
+    self.modNames=[]
     # self.multiplier=multiplier
   def addModifier(self, modifier):
     self.modifiers.append(modifier)
     self.multiplier=modifier.multiplier
     self.unit=modifier.unit
+    # self.values=modifier.values
   def __str__(self):
     out="Group Name: {}, ".format(self.name)
     for mod in self.modifiers:
@@ -360,17 +423,22 @@ class ModifierSubGroup(Modifier):
 
 
   def toStaticModifierFiles(self, modifierTagList, locList, rangeUsed=range(-2,11)):
+    if 0 in rangeUsed:
+      rangeUsed=list(rangeUsed)
+      rangeUsed.remove(0)
     self.rangeUsed=rangeUsed
     for m in rangeUsed:
       if m==0:
         continue
       modName="custom_difficulty_MM_{}_{!s}".format(self.name.replace(" ","_"), m)
+      self.modNames.append(modName)
       modifierList=modifierTagList.addReturn(modName)
       locList.append(modName, "$FE_DIFFICULTY:$"+self.name)
       for mod in self.modifiers:
         mod.toStaticModifierFiles(modifierList,None, range(m,m+1))
     modOptionName="custom_difficulty_MM_mod_{}".format(self.name.replace(" ","_"))
     locList.append(modOptionName, "§B"+self.name+"§!")
+    self.modifiername=modOptionName
     return modOptionName
 
 if __name__ == "__main__":
